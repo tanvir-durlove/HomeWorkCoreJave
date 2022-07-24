b445e5e22ff71b0bc6b41ac25efac341b743c4d4
Tanvir Durlove <Tanvir Durlove@DESKTOP-KFGAB3U.Starry-inc.net>
2022-07-24 00:32:13 -0700

    HomeWorkCoreJava : first commit

---
+++ HomeWorkCoreJava/src/hw2JavaBasics/MyCar.java
@@ -0,0 +1,8 @@
+package hw2JavaBasics;
+
+public class MyCar {
+	public static void main(String[] args) {
+		System.out.print("My Name is \"Tanvir\".\n" + "I love to drive.\n" + "My car info is below: \t");
+		System.out.print("Color : Black\n" + "Model : Hundai Accord\n" + "Year : 2020\n" + "Wheel drive : 2WD");
+	}
+}
