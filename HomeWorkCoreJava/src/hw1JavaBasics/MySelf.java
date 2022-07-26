54fcbef31517c06bc0a43d7be92c8bdc9f8653ab
Tanvir Durlove <Tanvir Durlove@DESKTOP-KFGAB3U.Starry-inc.net>
2022-07-25 21:48:56 -0700

    Java basic, class 01

---
+++ HomeWorkCoreJava/src/hw1JavaBasics/MySelf.java
@@ -0,0 +1,20 @@
+package hw1JavaBasics;
+
+/*for a project --> January_2022_core_java 
+for a package --> lec01_java_basics
+for a class --> Naming_conventions*/
+
+public class MySelf {
+
+//	Entry point for Java. 
+//	static method
+//	actually method name is main
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My Name is Tanvir");
+		System.out.println("Address : 269 S Lafayette Park Place, Los Angeles 90057, California");
+
+	}
+
+}
